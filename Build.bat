145cbfac5e0ba9f49c9fc319fbda74c0d78a588b
Zhongkui Li <v-zhonl@microsoft.com>
2019-02-27 10:01:05 +0800

    refactor

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 cd ./tools
-".nuget/NuGet.exe" install Rosalia -ExcludeVersion -OutputDirectory "./"
-"./Rosalia/tools/Rosalia.exe" /wd="../src" /task=BuildPackages "../tools/CrystalQuartz.Build/CrystalQuartz.Build.csproj"
+".nuget/NuGet.exe" install Rosalia -ExcludeVersion -OutputDirectory "./packages"
+"./packages/Rosalia/tools/Rosalia.exe" /wd="../src" /task=BuildPackages "../tools/CrystalQuartz.Build/CrystalQuartz.Build.csproj"
 pause
--- BuildAndDeploy.bat
+++ BuildAndDeploy.bat
@@ -1,4 +1,4 @@
 cd ./tools
-".nuget/NuGet.exe" install Rosalia -ExcludeVersion -OutputDirectory "./"
-"./Rosalia/tools/Rosalia.exe" /wd="../src" /hold /task="PushPackages" "../tools/CrystalQuartz.Build/CrystalQuartz.Build.csproj"
+".nuget/NuGet.exe" install Rosalia -ExcludeVersion -OutputDirectory "./packages"
+"./packages/Rosalia/tools/Rosalia.exe" /wd="../src" /hold /task="PushPackages" "../tools/CrystalQuartz.Build/CrystalQuartz.Build.csproj"
 pause
